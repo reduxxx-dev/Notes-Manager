d340777b5bcb86a904bc2e192a63b93c2190b5ca
reduxxx-dev <reduxxx123@gmail.com>
2026-04-23 09:54:16 +0200

    Full CRUD and authentication

--- login.php
+++ login.php
@@ -1,3 +1,34 @@
+
+<?php
+session_start();
+require_once "config/Database.php";
+
+$database = new Database();
+$db = $database->connect();
+
+$error = "";
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $stmt = $db->prepare("SELECT * FROM users WHERE username = :username");
+    $stmt->bindParam(':username', $username);
+    $stmt->execute();
+
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['user'] = $user['username'];
+        header("Location: index.php");
+        exit();
+    } else {
+        $error = "Wrong username or password";
+    }
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -49,3 +80,7 @@
 
 </body>
 </html>
+
+<p style="color:red; text-align:center;">
+    <?php if (!empty($error)) echo $error; ?>
+</p>
